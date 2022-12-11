9cb4b8aeb94ebad961c45c8618f1ed46c59b9497
acarni01 <acarni01@ucm.es>
2022-12-11 01:30:26 +0100

    Marge no me machaques los cambios marrrgeeee

---
+++ Assets/Scripts/Npcs/Guitarra.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guitarra : MonoBehaviour
+{
+    [SerializeField]
+    FMODUnity.StudioEventEmitter emisor_guitarra;
+
+    bool apagada = false;
+    private float timer;
+    private float tiempo_guitarra = 3;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = tiempo_guitarra;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (apagada)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                apagada = false;
+                emisor_guitarra.Play();
+                timer = tiempo_guitarra;
+            }
+        }
+    }
+
+    public void apagar_guitarra()
+    {
+        emisor_guitarra.EventInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        apagada = true;
+    }
+}
